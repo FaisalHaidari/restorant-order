bea5f14be1f9da66750f6a4c90bb1846724b8646
faisal <majidfaisal3@Gmail.com>
2025-05-13 22:11:11 +0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,19 @@ import Link from "next/link";
 export default function Home() {
   return (
 <>
-<header>
-  <Link className = "text-red-500 font-semibold text-2xl"href="">ATAKUM KAVURMACISI</Link>
-</header>
-<nav>
+<header className="flex items-center justify-between ">
+  <Link className = "text-orange-500 font-semibold text-2xl"href="">
+  ATAKUM KAVURMACISI
+  </Link>
+
+<nav className="flex items-center gap-8 text-gray-500 font-semibold">
   <Link href={""}>Anasayfa</Link>
   <Link href={""}>Menu</Link>
   <Link href={""}>Hakkında</Link>
   <Link href={""}>ILETISIM</Link>
-  <Link href={""}>Giriş Yap</Link>
+  <Link href={""} className="bg-orange-500 rounded-full text-white px-8 py-2">Giriş Yap</Link>
 </nav>
+</header>
 </>
   );
 }
