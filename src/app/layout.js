96e1c2bc413c1b71a5cbe06d99cae27ae9867d19
faisal <majidfaisal3@Gmail.com>
2025-05-13 21:38:40 +0300

    new navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
 
 export const metadata = {
   title: "Create Next App",
@@ -19,11 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${roboto.variable}`}>
+        <main className="max-w-4xl mx-auto border">
+          {children}
+        </main>
       </body>
     </html>
   );
 }
+ 
