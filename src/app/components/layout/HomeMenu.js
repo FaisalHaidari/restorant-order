3fc5e519c92f35462b343c90e4f57e864371b466
faisal <majidfaisal3@Gmail.com>
2025-05-16 22:26:47 +0300

    new home page

--- src/app/components/layout/HomeMenu.js
+++ src/app/components/layout/HomeMenu.js
@@ -1,20 +1,35 @@
 import Image from "next/image";
+import MenuItem from "../menu/MenuItem";
+import SectionHeaders from "./SectionHeaders";
+
+
 
 export default function HomeMenu() {
-    return (
-        <section className="">
-            <div className="relative">
-                <div className="h-60 w-60 absolute left-0 top-0 ">
-                    <Image src={'/rize.jpeg'} layout={'fill'} objectFit={'contain'} alt={'Rize'} />
-                </div>
-                <div className="h-60 w-60 absolute right-0 top-0 ">
-                    <Image src={'/hamburger.jpeg'} layout={'fill'} objectFit={'contain'} alt={'Hamburger'} />
-                </div>
-                <div className="text-center ">
-                    <h3 className="uppercase text-gray-600 font-semibold leading-3">Göz Atmak</h3>
-                    <h2 className="text-orange-500 font-bold text-4xl italic">Menü</h2>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="">
+      <div className="absolute  left-0 right-0 w-full justify-start">
+        <div className="absolute left-0 -top-[70px] text-left -z-10">
+          <Image src={'/spanak-l.png'} width={109} height={189} alt={'sallad'} />
+        </div>
+        <div className="absolute -top-[100px] right-0 -z-10">
+          <Image src={'/spanak-r.png'} width={107} height={195} alt={'sallad'} />
+        </div>
+      </div>
+
+      <div className="text-center mb-4">
+     <SectionHeaders subHeader={"Goz atmak"} mainHeader={"Menü"} />
+
+
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        < MenuItem />
+        < MenuItem />
+        < MenuItem />
+        < MenuItem />
+        < MenuItem />
+        < MenuItem />
+      </div>
+
+    </section>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
 import HomeMenu from "./components/layout/HomeMenu";
+import SectionHeaders from "./components/layout/SectionHeaders";
 
 export default function Home() {
   return (
@@ -9,6 +10,36 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
+      <section className="text-center mt-16">
+        <sectionHeaders 
+        subHeader ={'Our story '}
+        mainHeader ={'About Us'}/>
+       
+       <div className="text-gray-500 max-w-md mx-auto mt-6 flex-col gap-4">
+        <p>
+          Atakum Kavurmacısı, 1995 yılında kuruldu ve o zamandan beri
+          mükemmel kavurma sunma konusunda kendini adamıştır. Geleneksel
+          tariflerimizle hazırlanan kavurmamız, taze ve kaliteli malzemelerle
+          yapılmaktadır. Müşterilerimize en iyi deneyimi sunmak için sürekli
+          çalışıyoruz.
+        </p>
+       </div>
+      </section>
+      <section className="text-center my-8">
+        <sectionHeaders 
+        subHeader ={'DON\'T MISS OUT'}
+        mainHeader ={'Contact us'}/>
+       <div className="mt-8">
+        <a className="text-4xl underline text-gray-500 "
+        href ="tel:+905555555555">
+          +90 555 555 55 55
+        </a>
+       </div>
+      </section>
+      <footer className="border-t P-8 text-center text-gray-500 mt-16"> 
+        &copy; 2023 Atakum Kavurmacısı. Tüm hakları saklıdır.
+      
+      </footer>
 </>
   );
 }
